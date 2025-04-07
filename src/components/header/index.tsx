79550aea96d16ada4c2a09c5f312741852cb27d0
PhTheDev <fariagm123@gmail.com>
2025-04-07 16:39:25 -0300

    feat: add Header component with navigation links and logo

---
+++ src/components/header/index.tsx
@@ -0,0 +1,34 @@
+import Image from "next/image";
+import Link from "next/link";
+import { LiaGamepadSolid } from "react-icons/lia";
+import logoImg from "../../../public/logo.svg";
+
+export function Header() {
+  return (
+    <header className="w-full h-28 bg-slate-100 text-black px-2">
+      <div className="max-w-screen-xl mx-auto flex justify-center items-center h-28 sm:justify-between">
+        <nav className="flex justify-center items-center gap-4">
+          <Link href="/">
+            <Image
+              src={logoImg}
+              alt="logo"
+              quality={100}
+              priority={true}
+              width={150}
+              height={60}
+              className="w-full"
+            />
+          </Link>
+          <Link href="/games">Games</Link>
+          <Link href="/profile">Perfil</Link>
+        </nav>
+
+        <div className="hidden sm:flex justify-center items-center">
+          <Link href="/login">
+            <LiaGamepadSolid size={34} color="#475569" />
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
+}
